2334dedc4d446efed7757b07c4848498c1eb2645
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-06-13 13:07:40 +0300

    Install soci dependancies via vcpkg and establish connection to MSSQL using ODBC backend.

--- gradebook/gradebook.cpp
+++ gradebook/gradebook.cpp
@@ -1,46 +1,22 @@
-
+#include <soci/soci.h>
+#include <soci/odbc/soci-odbc.h>
 #include <iostream>
-#include <vector>
-
-#include "./odbc_helper.hpp"
+#include <string>
 
 int main()
 {
-    //1. Declare ODBC handles
+	try {
+		soci::session sql(soci::odbc, "DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-3KAPKMM;DATABASE=TestDB;UID=sa;PWD=sa;");
 
-    SQLHENV hEnv = nullptr;     //Environment handle
-    SQLHDBC hDbc = nullptr;     //Database connection handle (knex)
-    SQLHSTMT hStmt = nullptr;   //Statement handle 
-    
-    //2. Allocate environment handle
-    if (!SQL_SUCCEEDED(allocEnvironment(hEnv))) {
-        std::cerr << "Env alloc failed\n"; 
-        return 1;
-    }
-
-    //3. Allocate connection handle
-
-    if (!SQL_SUCCEEDED(allocConnection(hEnv, hDbc))) {
-        std::cerr << "Connection alloc failed\n"; 
-        cleanUpHandles(hDbc, hEnv); 
-        return 1;
-    }
-
-    SQLWCHAR connStr[] = L"DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-3KAPKMM;DATABASE=TestDB;UID=sa;PWD=sa;";
-    //for now 
-
-    if (!SQL_SUCCEEDED(connectDatabase(hDbc, connStr))) {
-        std::cerr << "Connect failed\n"; cleanUpHandles(hDbc, hEnv); 
-        return 1;
-    }
-
-    std::cout << "Connected to SQL Server!\n";
-
-    //Statement logic
+		std::cout << "------------------------\n";
+		std::cout << "Connected via SOCI-ODBC!\n";
+		std::cout << "------------------------\n";
 
 
-    cleanUpHandles(hDbc, hEnv);
-
-
-    return 0;
+	}
+	catch (const soci::soci_error& e) {
+		std::cerr << "Connection failed: " << e.what() << "\n";
+		return 1;
+	}
+	return 0;
 }
