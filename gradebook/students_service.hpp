6e380d2c38c9a5aaf9b43c29f62caa7ce0070589
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-06-13 19:07:14 +0300

    Implement service logic for all entities namely basic CRUD methods; Inititate the implementation of a CLI App class; To be done a ReportingService which handles complex SQL query logic.

--- gradebook/students_service.hpp
+++ gradebook/students_service.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+class database_error : public std::runtime_error{
+public:
+	database_error(const std::string& msg) : std::runtime_error(msg) {}
+};
+
 struct Student {
 	int id;
 	int number;
@@ -16,7 +21,7 @@ public:
 		std::cout << "Class number: " << s.number << "\n";
 		std::cout << "Full Name:    " << s.name << "\n";
 		std::cout << "Birthdate:    " << s.birthdate << "\n";
-		std::cout << "-----------------------" << "\n";
+		std::cout << "--------------------------" << "\n";
 	}
 
 	static void print(const std::vector<Student>& students) {
@@ -27,7 +32,7 @@ public:
 			std::cout << "Class number: " << s.number << "\n";
 			std::cout << "Full Name:    " << s.name << "\n";
 			std::cout << "Birthdate:    " << s.birthdate << "\n";
-			std::cout << "-----------------------" << "\n";
+			std::cout << "--------------------------" << "\n";
 		}
 	}
 
@@ -51,5 +56,52 @@ public:
 
 		return students;
 	}
-};
+
+	static void insert(soci::session& sql, const Student& s) {
+
+		sql << "INSERT INTO students (number, name, birthdate) VALUES (:number, :name, :birthdate)", soci::use(s.number), soci::use(s.name), soci::use(s.birthdate);
+	}
+
+	static Student findById(soci::session& sql, int id) {
+		Student s;
+		soci::statement stmt = (sql.prepare << "SELECT id, number, name, birthdate FROM students WHERE id = :id", soci::use(id),
+			soci::into(s.id),
+			soci::into(s.number),
+			soci::into(s.name),
+			soci::into(s.birthdate));
+
+		stmt.execute();
+
+		if (!stmt.fetch()) {
+			std::string message = "Non-existent student with id " + std::to_string(id) + "!";
+			throw database_error(message);
+		} 
+
+		return s;
+	}	
+
+	static void editById(soci::session& sql,int id, const Student& newStudent) {
+
+		findById(sql, id);
+
+		sql << "UPDATE students SET number = :number, name = :name, birthdate = :birthdate WHERE id = :id",
+			soci::use(newStudent.number),
+			soci::use(newStudent.name),
+			soci::use(newStudent.birthdate),
+			soci::use(id);
+
+		std::cout << "Successfully edited student with id " + std::to_string(id) + "!\n";
+		std::cout << "---------------------------\n";
+	}
+
+	static void deleteById(soci::session& sql, int id) {
+
+		Student s = findById(sql, id);
+
+		sql << "DELETE FROM students WHERE id = :id", soci::use(id);
+
+		std::cout << "Successfully deleted student " + s.name + "!\n";
+		std::cout << "---------------------------\n";
+	}
+};	
 
---
+++ gradebook/subjects_service.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+struct Subject {
+	int id;
+	int number;
+	std::string name;
+	std::string teacher;
+};
+
+class SubjectService {
+	SubjectService() = delete;
+
+public:
+	static void print(const Subject& s) {
+		std::cout << "Subject id:   " << s.id << "\n";
+		std::cout << "Room number:  " << s.number << "\n";
+		std::cout << "Subject name: " << s.name<< "\n";
+		std::cout << "Teacher:		" << s.teacher << "\n";
+		std::cout << "--------------------------" << "\n";
+	}
+
+	static void print(const std::vector<Subject>& subjects) {
+
+		for (int i = 0; i < subjects.size(); ++i) {
+			Subject s = subjects[i];
+			std::cout << "Student id:   " << s.id << "\n";
+			std::cout << "Class number: " << s.number << "\n";
+			std::cout << "Full Name:    " << s.name << "\n";
+			std::cout << "Teacher:    " << s.teacher << "\n";
+			std::cout << "--------------------------" << "\n";
+		}
+	}
+
+	static std::vector<Subject> fetchAll(soci::session& sql) {
+
+		std::vector<Subject> subjects;
+
+		Subject s;
+
+		soci::statement st = (sql.prepare << "SELECT id, number, name, teacher FROM subjects",
+			soci::into(s.id),
+			soci::into(s.number),
+			soci::into(s.name),
+			soci::into(s.teacher));
+
+
+		st.execute();
+		while (st.fetch()) {
+			subjects.push_back(s);
+		}
+
+		return subjects;
+	}
+
+	static void insert(soci::session& sql, const Subject& s) {
+		sql << "INSERT INTO subjects (number, name, teacher) VALUES (:number, :name, :teacher)", soci::use(s.number), soci::use(s.name), soci::use(s.teacher);
+
+
+		std::cout << " Successful insertion of subject " + s.name + "!\n";
+		std::cout << "--------------------------\n";
+	}
+
+	static Subject findById(soci::session& sql, int id) {
+		Subject s;
+		soci::statement stmt = (sql.prepare << "SELECT id, number, name, teacher FROM subjects WHERE id = :id", soci::use(id),
+			soci::into(s.id),
+			soci::into(s.number),
+			soci::into(s.name),
+			soci::into(s.teacher));
+
+		stmt.execute();
+
+		if (!stmt.fetch()) {
+			std::string message = "Non-existent subject with id " + std::to_string(id) + "!";
+			throw database_error(message);
+		}
+
+		return s;
+	}
+
+	static void editById(soci::session& sql, int id, const Subject& newSubject) {
+
+		findById(sql, id);
+
+		sql << "UPDATE subjects SET number = :number, name = :name, teacher = :teacher WHERE id = :id",
+			soci::use(newSubject.number),
+			soci::use(newSubject.name),
+			soci::use(newSubject.teacher),
+			soci::use(id);
+
+		std::cout << "Successfully edited subject with id " + std::to_string(id) + "!\n";
+		std::cout << "------------------------\n";
+	}
+
+	static void deleteById(soci::session& sql, int id) {
+
+		Subject s = findById(sql, id);
+
+		sql << "DELETE FROM subjects WHERE id = :id", soci::use(id);
+
+		std::cout << "Successfully deleted subject " + s.name + "!\n";
+		std::cout << "------------------------\n";
+	}
+};
+
